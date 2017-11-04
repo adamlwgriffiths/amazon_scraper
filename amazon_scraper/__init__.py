442929a64cd120f1066a2eff0b7d50de0d2da2d7
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2017-11-04 10:37:37 +0100

    Parse dates for foreign regions.

--- amazon_scraper/__init__.py
+++ amazon_scraper/__init__.py
@@ -139,6 +139,7 @@ def add_query(url, **kwargs):
 
 def get_review_date(raw_date):
     string = unicode(raw_date)
+    string = ' '.join(string.split(' ')[1:])
     # 2011-11-07T05:50:41Z
     date = dateutil.parser.parse(string)
     return date
